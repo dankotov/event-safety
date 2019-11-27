f8fdb483991f306bd1d0edffdb8ce8948c0bcdcd
Dan Kotov <dkotov@ryerson.ca>
2019-11-26 19:43:54 -0500

    upload content

---
+++ map-app/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import Map from './Map';
+
+const OuterWrapper = styled.div`
+  display: table; 
+  position: absolute;
+  height: 100%;
+  width: 100%;
+`;
+
+const AppWrapper = styled.div`
+  display: table-cell;
+  vertical-align: middle;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: auto auto;
+`;
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checked: false,
+    }
+
+  }
+
+  render() {
+    return (
+      <OuterWrapper>
+        <AppWrapper>
+          <Container>
+            <Map />
+          </Container>
+        </AppWrapper>
+      </OuterWrapper>
+    );
+  }
+
+}
